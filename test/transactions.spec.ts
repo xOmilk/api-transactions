ace3c3c75979e0ae9cfadb876f1826f3abd6424f
xOmilk <leite46052@gmail.com>
2025-10-21 22:52:35 -0300

    test: add unit test for listing all transactions with cookie support

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from "vitest";
+import { test, beforeAll, afterAll, describe, it, expect } from "vitest";
 import request from "supertest";
 import { app } from "../src/app";
 
@@ -20,5 +20,35 @@ describe("Transactions routes", () => {
 				type: "credit",
 			})
 			.expect(201);
+	});
+
+	it("should be able to list all transactions", async () => {
+		//para listar uma transação é preciso criar uma primeiramente
+
+		const createTransaction = await request(app.server)
+			.post("/transactions")
+			.send({
+				title: "New Transaction",
+				amount: 5000,
+				type: "credit",
+			});
+
+		const cookies = createTransaction.get("Set-Cookie");
+
+		if (!cookies) throw new Error("No cookies returned from transaction");
+
+		const allTransactionsResponse = await request(app.server)
+			.get("/transactions")
+			.set("Cookie", cookies)
+			.expect(200);
+
+		expect(allTransactionsResponse.body.transactions).toEqual([
+			expect.objectContaining({
+				title: "New Transaction",
+				amount: 5000,
+			})
+		])
+
+
 	});
 });
