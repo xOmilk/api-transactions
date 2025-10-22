391673dbae2b84e0e8c3e3ebf14a7c12b34ee122
xOmilk <leite46052@gmail.com>
2025-10-21 23:50:37 -0300

    test: add unit tests for retrieving specific transactions and transaction summaries

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -37,6 +37,41 @@ describe("Transactions routes", () => {
 			.expect(201);
 	});
 
+	it("should be able to get a specific transaction", async () => {
+		//para listar uma transação é preciso criar uma primeiramente
+
+		const createTransaction = await request(app.server)
+			.post("/transactions")
+			.send({
+				title: "New Transaction",
+				amount: 5000,
+				type: "credit",
+			});
+
+		const cookies = createTransaction.get("Set-Cookie");
+
+		if (!cookies) throw new Error("No cookies returned from transaction");
+
+		const allTransactionsResponse = await request(app.server)
+			.get("/transactions")
+			.set("Cookie", cookies)
+			.expect(200);
+
+		const transactionId = allTransactionsResponse.body.transactions[0].id;
+
+		const getATransactionsResponse = await request(app.server)
+			.get(`/transactions/${transactionId}`)
+			.set("Cookie", cookies)
+			.expect(200);
+
+		expect(getATransactionsResponse.body.transaction).toEqual(
+			expect.objectContaining({
+				title: "New Transaction",
+				amount: 5000,
+			})
+		);
+	});
+
 	it("should be able to list all transactions", async () => {
 		//para listar uma transação é preciso criar uma primeiramente
 
@@ -63,5 +98,37 @@ describe("Transactions routes", () => {
 				amount: 5000,
 			}),
 		]);
+	});
+
+	test("the user should be able to get the summary of his transactions", async () => {
+		const firstTransaction = await request(app.server)
+			.post("/transactions")
+			.send({
+				title: "User first transaction",
+				amount: 1000,
+				type: "credit",
+			});
+
+		const cookies = firstTransaction.get("Set-Cookie");
+
+		if (!cookies) throw new Error("No cookies returned from transaction");
+
+		const theSecondTransaction = await request(app.server)
+			.post("/transactions")
+			.set("Cookie", cookies)
+			.send({
+				title: "User first transaction",
+				amount: 1000,
+				type: "credit",
+			});
+
+		const summaryUserTransactions = await request(app.server)
+			.get("/transactions/summary")
+			.set("Cookie", cookies)
+			.expect(200);
+
+		expect(summaryUserTransactions.body.summary).toEqual({
+			totalAmount: 2000,
+		});
 	});
 });
