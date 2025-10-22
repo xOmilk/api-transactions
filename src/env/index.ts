e35f7e77c52ed1fc509b18ac74fd8ad20dbb8652
xOmilk <leite46052@gmail.com>
2025-10-22 00:30:34 -0300

    feat: update environment configuration for PostgreSQL support and add pg dependency

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,20 +1,19 @@
 import { config } from "dotenv";
 import { z } from "zod";
 
-
-if (process.env.NODE_ENV === 'test'){
-	config({path:'.env.test'})
-}
-else{
-	config()
+if (process.env.NODE_ENV === "test") {
+	config({ path: ".env.test" });
+} else {
+	config();
 }
 
 const envSchema = z.object({
 	NODE_ENV: z
 		.enum(["production", "test", "development"])
 		.default("production"),
+	DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
 	DATABASE_URL: z.string(),
-	PORT: z.number().default(3333),
+	PORT: z.coerce.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
