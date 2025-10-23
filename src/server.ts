64434dc441fd8c03516e647be2615ac3372299e7
xOmilk <leite46052@gmail.com>
2025-10-22 23:25:03 -0300

    fix: specify host in server listen configuration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 import { env } from "./env";
 
-app.listen({ port: env.PORT }).then(() =>
+app.listen({ host: "0.0.0.0", port: env.PORT }).then(() =>
 	console.log("Server Running at port:3333")
 );
